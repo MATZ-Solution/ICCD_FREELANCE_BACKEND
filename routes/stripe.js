1f5fa26e5231798aec4ac8ac5e16d4d91ad68ff7
Syed Mohiuddin <smohiuddinh@gmail.com>
2025-08-01 14:45:17 +0500

    stripe controller changes

--- routes/stripe.js
+++ routes/stripe.js
@@ -5,5 +5,8 @@ const stripeController = require("../controllers/stripeController");
 router.post("/create-checkout-session", stripeController.createCheckoutSession);
 router.get("/session", stripeController.getSession);
 router.post("/process-order", stripeController.processOrder);
+router.get("/getAllOrder", stripeController.getAllOrders);
+router.get("/getAllOrderByFreelancer/:freelancerId", stripeController.getAllOrderByFreelancer);
+router.get("/getSingleOrderByFreelancer/:orderId", stripeController.getSingleOrderByFreelancer);
 
 module.exports = router;
