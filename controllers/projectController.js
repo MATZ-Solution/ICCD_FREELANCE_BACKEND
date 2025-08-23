84b467192f513de4cc2a218f692a343ffc86268d
sohaibnayyar721 <sohaibnayyar.721@gmail.com>
2025-08-23 11:17:23 +0500

    changes in job detail api

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -175,7 +175,8 @@ exports.getAllProject = async (req, res) => {
 };
 
 exports.getProjectById = async (req, res) => {
-  const { projectId } = req.params;
+  const { projectId, freelancerId = null } = req.query;
+
   try {
     const getProjectQuery = `
     SELECT  p.*, GROUP_CONCAT(DISTINCT ps.name) AS skills, GROUP_CONCAT(DISTINCT pl.name) AS languages,
@@ -189,11 +190,32 @@ exports.getProjectById = async (req, res) => {
     const selectResult = await queryRunner(getProjectQuery, [projectId]);
 
     if (selectResult[0].length > 0) {
-      res.status(200).json({
-        statusCode: 200,
-        message: "Success",
-        data: selectResult[0]
-      });
+      if(freelancerId){
+        const appliedQuery = `SELECT projectId, freelancerId FROM project_proposals WHERE projectId = ? AND freelancerId = ? `
+        const selectResultApplied = await queryRunner(appliedQuery, [projectId, freelancerId])
+        if (selectResultApplied[0].length > 0) {
+          res.status(200).json({
+            statusCode: 200,
+            message: "Success",
+            data: selectResult[0],
+            applied: true
+          });
+        } else {
+          res.status(200).json({
+            statusCode: 200,
+            message: "Success",
+            data: selectResult[0],
+            applied: false
+          });
+        }
+      }else{
+         res.status(200).json({
+            statusCode: 200,
+            message: "Success",
+            data: selectResult[0],
+          });
+      }
+
     } else {
       res.status(200).json({
         data: [],
