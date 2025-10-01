3e940c817f20a945934de072a64bce5bef83135d
sohaibnayyar721 <sohaibnayyar.721@gmail.com>
2025-10-02 01:56:37 +0500

    changes in s3 configuration

--- config/s3.js
+++ config/s3.js
@@ -1,15 +1,15 @@
 // config.js
+module.exports = {
+    bucketName: 'iccdfreelancematzsolutionsbucket',
+    awsAccessKeyId: "AKIAYBQVPHFK2FTPMPGN",
+    awsSecretAccessKey: "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",
+    region: 'eu-north-1',
+  };
+
 // module.exports = {
 //     bucketName: 'powerhouseassets',
-//     awsAccessKeyId: "AKIAQPZG37TRU4ABDSXN",
-//     awsSecretAccessKey: "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",
+//     awsAccessKeyId: "AKIAQPZG37TR6DHUMMSD",
+//     awsSecretAccessKey: "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",
 //     region: 'us-east-1',
 //   };
-
-module.exports = {
-    bucketName: 'powerhouseassets',
-    awsAccessKeyId: "AKIAQPZG37TR6DHUMMSD",
-    awsSecretAccessKey: "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",
-    region: 'us-east-1',
-  };
   
