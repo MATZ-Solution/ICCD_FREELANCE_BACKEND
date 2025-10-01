c7cf41a6a01503055f274a479a40d0b980054a37
sohaibnayyar721 <sohaibnayyar.721@gmail.com>
2025-10-02 02:30:31 +0500

    changes in s3 files

--- config/s3.js
+++ config/s3.js
@@ -1,10 +1,4 @@
 // config.js
-// module.exports = {
-//     bucketName: 'powerhouseassets',
-//     awsAccessKeyId: "AKIAQPZG37TRU4ABDSXN",
-//     awsSecretAccessKey: "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",
-//     region: 'us-east-1',
-//   };
 
 module.exports = {
     bucketName: 'iccdfreelancematzsolutionsbucket',
