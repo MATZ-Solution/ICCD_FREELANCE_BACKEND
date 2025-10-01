32e0c91bb52eeab778d411d03ec2634005536021
sohaibnayyar721 <sohaibnayyar.721@gmail.com>
2025-10-02 02:24:48 +0500

    changes in s3 configuration

--- config/s3.js
+++ config/s3.js
@@ -1,2 +1,7 @@
 // config.js
-
+module.exports = {
+    bucketName: 'iccdfreelancematzsolutionsbucket',
+    awsAccessKeyId: "AKIAYBQVPHFK2FTPMPGN",
+    awsSecretAccessKey: "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",
+    region: 'eu-north-1',
+  };
